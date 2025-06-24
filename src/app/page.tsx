9c4fb978e7330896f05434154fd5126f8794be9d
Kaufko <99084056+Kaufko@users.noreply.github.com>
2025-06-24 19:10:28 +0200

    Removed test and changed it into main page (now vertically scrollable)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,64 @@
-﻿"use client";
-import { motion } from "framer-motion";
+﻿'use client';
+// next/dynamic skips SSR for this component
+import dynamic from 'next/dynamic'; // Add this import
+import { lazy ,Suspense } from "react";
 
-const HomeText = `Hey, I'm Filip. I make websites, and 3D models`;
-const LocationText = `Based in the Czech Republic`;
-const Landing = () => {
+const RenderOnViewportEntry = dynamic(() => import('@/app/components/RenderOnViewportEntry'), { 
+  ssr: false 
+});
 
+const HomePage = lazy(() => import("@/app/home/page"));
+const AboutPage = lazy(() => import("@/app/about/page"));
+const ProjectsPage = lazy(() => import("@/app/projects/page"));
+const ContactPage = lazy(() => import("@/app/contact/page"));
+import Loading from "@/app/loading";
 
-    return (
-        <div className="bg-center bg-cover bg-no-repeat min-h-screen bg-[url('/images/bg2.png')]">
-            <motion.div className="flex min-h-screen flex-col justify-center text-center"
-                initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
-                <ul className="flex flex-col justify-between">
-                    <li className="font-bold lg:p-5 lg:text-xl max-lg:p-1 max-lg:text-sm">
-                        {LocationText}
-                    </li>
-                    <li className="lg:text-5xl max-lg:text-2xl">
-                        Quality <strong className="text-[var(--highlight-midsat)]">web design &  model <br></br> topology</strong> techniques
-                    </li>
-                    <li className="lg:p-5 max-lg:p-1 max-lg:text-xs">
-                        {HomeText}
-                    </li>
-                    
-                </ul>
-                
-            </motion.div>
-        </div>
-    );
+interface ROVEOptionsType {
+  threshold?: number;
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+const scrollROVEOptions: ROVEOptionsType = {
+  threshold: 0.01,
+  className: "snap-start h-screen"
 };
 
-export default Landing;
+export default function Home() {
+  return (
+    <main
+      className="h-screen overflow-y-scroll scroll-smooth snap-y snap-mandatory" style={{ scrollSnapType: 'y mandatory', scrollBehavior: 'smooth' }}>
+      <section className="snap-start h-screen">
+        <RenderOnViewportEntry {...scrollROVEOptions}>
+          <Suspense fallback={<Loading />}>
+            <HomePage />
+          </Suspense>
+        </RenderOnViewportEntry>
+      </section>
+
+      <section className="snap-start h-screen">
+        <RenderOnViewportEntry {...scrollROVEOptions}>
+          <Suspense fallback={<Loading />}>
+            <AboutPage />
+          </Suspense>
+        </RenderOnViewportEntry>
+      </section>
+
+      <section className="snap-start h-screen">
+        <RenderOnViewportEntry {...scrollROVEOptions}>
+          <Suspense fallback={<Loading />}>
+            <ProjectsPage />
+          </Suspense>
+        </RenderOnViewportEntry>
+      </section>
+
+      <section className="snap-start h-screen">
+        <RenderOnViewportEntry {...scrollROVEOptions}>
+          <Suspense fallback={<Loading />}>
+            <ContactPage />
+          </Suspense>
+        </RenderOnViewportEntry>
+      </section>
+    </main>
+  );
+}
