0c3769a55946f1a7a45d4de8420c5fc0cf801a5a
Kaufko <99084056+Kaufko@users.noreply.github.com>
2025-06-24 19:10:28 +0200

    Changed layout.tsx to fit vertical page scrolling.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,31 +17,9 @@ export default function RootLayout({ children, }:
     return (
         <html lang="en">
             <body>
-                <header className="fixed top-0 z-50 w-full">
-                    <div className="mx-auto flex max-w-6xl items-center justify-between p-4">
-                        <h1 className="font-bold max-lg:text-xs">Filip H.</h1>
-                        <nav>
-                            <ul className="flex lg:space-x-6">
-                                <li className="route-header-li">
-                                    <Link href="/" className="route-header-link">Home</Link>
-                                </li>
-                                <li className="route-header-li">
-                                    <Link href="/projects" className="route-header-link">Projects</Link>
-                                </li>
-                                <li className="route-header-li">
-                                    <Link href="/about" className="route-header-link">About</Link>
-                                </li>
-
-                            </ul>
-                        </nav>
-                        <ul>
-                            <li className="route-header-li">
-                                <Link href="/contact" className="route-header-link">Contact</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </header>
+                <section className="snap-start h-screen" id="snap-section">
                 {children}
+                </section>
             </body>
         </html>
     )
